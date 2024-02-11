ba9bf59dd30435a9ef8f8f1f3c137207edd76c8c
isevou__ <kbry12@naver.com>
2024-02-11 18:36:34 +0900

    feat :예외 처리하고 반환하기 (Return and handle an error)

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -1,10 +1,18 @@
 package greetings
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 // Hello returns a greeting for the named person.
-func Hello(name string) string {
-	// Return a greeting that embeds the name in a message.
+func Hello(name string) (string, error) {
+	// 아무 이름도 안들어온다면, 메시지와 에러를 반환한당~
+	if name == "" {
+		return "", errors.New("empty name")
+	}
+
+	// 이름이 들어온다면, 이름을 담고 있는 값을 환영 메시지에 담아서 반환한당.
 	message := fmt.Sprintf("Hi, %v. Welcom!", name)
-	return message
+	return message, nil
 }
--- hello/hello.go
+++ hello/hello.go
@@ -1,13 +1,27 @@
 package main
 
 import (
-	"fmt"
-
 	"awesomeProject/greetings"
+	"fmt"
+	"log"
 )
 
 func main() {
-	// Get a greeting message and print it.
-	message := greetings.Hello("Gladys")
+	// 정해진 로거의 설정을 변경한다 ->
+	// 로그 메시지의 접두사를 정하고
+	// 플래그를 설정해서 시간, 소스 코드, 라인 넘버를 출력을 막는다.
+	// 플래그에 따라서 로그에 어떤 종류의 로깅을 남길 지 정할 수 있다.
+	//
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
+	// 환영 메시지를 요청한다.
+	message, err := greetings.Hello("")
+	// 예외가 발생하면, 콘솔에 출력하고 프로그램을 종료한다.
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// 아무 예외도 반환되지 않았다면, 반환된 메시지를 콘솔에 출력한다.
 	fmt.Println(message)
 }
