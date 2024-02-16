8b31e25c78cedd27265a243553ac23e65239d459
hodako <kbry12@naver.com>
2024-02-16 17:01:25 +0900

    refactor : 랜덤 뽑기

--- random/random_person.go
+++ random/random_person.go
@@ -1,1 +1,21 @@
-package random
+package main
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+func main() {
+	fmt.Println(getOne())
+}
+
+func getOne() string {
+	people := []string{
+		"주광",
+		"병룡",
+		"찬욱",
+		"원상",
+	}
+
+	return people[rand.Intn(len(people))]
+}
